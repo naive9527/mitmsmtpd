519ec919a31095c060bfd8e5fd77e96d13316822
naive9527 <naive9527@sina.com>
2025-04-21 17:17:58 +0800

    Added the authentication function

--- main.go
+++ main.go
@@ -2,81 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log/slog"
-	"net"
-	"strings"
 
-	"github.com/emersion/go-message"
-	gomail "github.com/emersion/go-message/mail" // Avoid conflicts with net/mail
-	"github.com/naive9527/mitmsmtpd/smtpd"       // It is actually a modified version of https://github.com/mhale/smtpd.
+	// Avoid conflicts with net/mail
+	"github.com/naive9527/mitmsmtpd/smtpd" // It is actually a modified version of https://github.com/mhale/smtpd.
 )
 
-func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
-	// err := SaveMail(data)
-	// if err != nil {
-	// 	return err
-	// }
+var AuthMechs = map[string]bool{"PLAIN": true, "LOGIN": true}
 
-	r := strings.NewReader(string(data))
-	msg, err := message.Read(r)
+func ListenAndServeTLSAuth(addr string, certFile string, keyFile string, handler smtpd.Handler, appname string, hostname string, authHandler smtpd.AuthHandler) error {
+	srv := &smtpd.Server{Addr: addr, Handler: handler, Appname: appname, Hostname: hostname, AuthHandler: authHandler, AuthRequired: true,
+		AuthMechs: AuthMechs}
+	err := srv.ConfigureTLS(certFile, keyFile)
 	if err != nil {
-		slog.Error(err.Error())
 		return err
 	}
-
-	// get mail header
-	mailHeader := gomail.Header{Header: msg.Header}
-	// fromList, _ := mailHeader.Text("From")
-	// to = mailHeader.Text("To") + mailHeader.Text("Cc")
-	toList, _ := mailHeader.Text("To")
-	ccList, _ := mailHeader.Text("Cc")
-	subject, _ := mailHeader.Subject()
-
-	fmt.Printf("Received an email From: %s To: %s. email header To: %s. email header Cc: %s. Subject is: %s", from, strings.Join(to, "; "), toList, ccList, subject)
-
-	// Handle the body of the email.
-	r = strings.NewReader(string(data))
-	body, err := gomail.CreateReader(r)
-	if err != nil {
-		slog.Error(err.Error())
-		return err
-	}
-
-	// Loop through reading each part of the body.
-	for {
-		p, err := body.NextPart()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			slog.Error(err.Error())
-			return err
-		}
-
-		switch h := p.Header.(type) {
-		case *gomail.InlineHeader:
-			// This is the message's text (can be plain-text or HTML)
-			b, _ := io.ReadAll(p.Body)
-			fmt.Println("Body:", string(b))
-		case *gomail.AttachmentHeader:
-			// This is an attachment (Including the attached files and the pictures in the document)
-			filename, err := h.Filename()
-			contentType := p.Header.Get("Content-Type")
-			if err != nil || filename == "" {
-				cid := strings.Trim(h.Get("Content-Id"), "<>")
-				filename = cid
-				fmt.Printf("The file embedded in the email body:%s type %s\n", filename, contentType)
-			} else {
-				fmt.Printf("Attachment:%s type %s\n", filename, contentType)
-			}
-
-		default:
-			slog.Info("Unknown header type")
-		}
-
-	}
-	return nil
+	return srv.ListenAndServe()
 }
 
 func main() {
@@ -86,8 +27,10 @@ func main() {
 	fmt.Printf("Starting SMTP server on %s\n", server)
 	// err := smtpd.ListenAndServe(":2525", mailHandler, "MyServerApp", "mail.example.com")
 	// err := smtpd.ListenAndServe(":2525", mailHandler, "MyServerApp", "")
-	err := smtpd.ListenAndServeTLS(server, certFile, keyFile, mailHandler, "MyServerApp", "")
+	// err := smtpd.ListenAndServeTLS(server, certFile, keyFile, mailHandler, "MyServerApp", "")
+	err := ListenAndServeTLSAuth(server, certFile, keyFile, mailHandler, "MyServerApp", "", authHandler)
 	if err != nil {
 		slog.Error(err.Error())
 	}
+
 }
--- utils.go
+++ utils.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"hash/fnv"
+	"io"
 	"log/slog"
 	"net"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/emersion/go-message"
+	gomail "github.com/emersion/go-message/mail"
 )
 
 func SaveMail(data []byte) error {
@@ -24,19 +29,90 @@ func hashSubject(data []byte) uint64 {
 
 // User database (stored in memory; in production environment, it should be replaced by a database)
 var userDB = map[string]string{
-	"user01@example.com": "securepassword123",
+	"user01@example.com": "123456",
 }
 
-// 认证回调函数
 func authHandler(remoteAddr net.Addr, mechanism string, username []byte, password []byte, shared []byte) (bool, error) {
+	value, ok := AuthMechs[mechanism]
+	if !(ok && value == true) {
+		slog.Info("Unsupported authentication method", mechanism)
+		return false, nil
+	}
 	user := string(username)
 	pass := string(password)
 
 	// 验证用户名和密码
 	if storedPass, ok := userDB[user]; ok && storedPass == pass {
-		slog.Info("Authentication successful", "Username", user)
+		slog.Info("Authentication successful", mechanism, "Username", user)
 		return true, nil
 	}
-	slog.Warn("Authentication failed", "Username", user)
+	slog.Warn("Authentication failed", mechanism, "Username", user)
 	return false, nil
+}
+
+func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
+	// err := SaveMail(data)
+	// if err != nil {
+	// 	return err
+	// }
+
+	r := strings.NewReader(string(data))
+	msg, err := message.Read(r)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	// get mail header
+	mailHeader := gomail.Header{Header: msg.Header}
+	// fromList, _ := mailHeader.Text("From")
+	// to = mailHeader.Text("To") + mailHeader.Text("Cc")
+	toList, _ := mailHeader.Text("To")
+	ccList, _ := mailHeader.Text("Cc")
+	subject, _ := mailHeader.Subject()
+
+	fmt.Printf("Received an email From: %s To: %s. email header To: %s. email header Cc: %s. Subject is: %s", from, strings.Join(to, "; "), toList, ccList, subject)
+
+	// Handle the body of the email.
+	r = strings.NewReader(string(data))
+	body, err := gomail.CreateReader(r)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	// Loop through reading each part of the body.
+	for {
+		p, err := body.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			slog.Error(err.Error())
+			return err
+		}
+
+		switch h := p.Header.(type) {
+		case *gomail.InlineHeader:
+			// This is the message's text (can be plain-text or HTML)
+			b, _ := io.ReadAll(p.Body)
+			fmt.Println("Body:", string(b))
+		case *gomail.AttachmentHeader:
+			// This is an attachment (Including the attached files and the pictures in the document)
+			filename, err := h.Filename()
+			contentType := p.Header.Get("Content-Type")
+			if err != nil || filename == "" {
+				cid := strings.Trim(h.Get("Content-Id"), "<>")
+				filename = cid
+				fmt.Printf("The file embedded in the email body:%s type %s\n", filename, contentType)
+			} else {
+				fmt.Printf("Attachment:%s type %s\n", filename, contentType)
+			}
+
+		default:
+			slog.Info("Unknown header type")
+		}
+
+	}
+	return nil
 }
